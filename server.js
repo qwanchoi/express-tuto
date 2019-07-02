6c61444e191428da55b64c5e6c83fc8fc9ec78d6
qwanchoi <chlrbdhks92@gmail.com>
2019-07-02 10:35:44 +0900

    ejs 기초 & 모듈 추가

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+// 참조 : https://velopert.com/379
+
 var express = require('express');
 var app = express();
-var router = require('./router/main') (app);
+var bodyParser = require('body-parser'); // POST 데이터 처리
+var session = require('express-session'); // 세션 관리, 쿠키 접근
+var fs = require('fs');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -9,3 +13,15 @@ app.engine('html', require('ejs').renderFile);
 var server = app.listen(3000, function() {
     console.log('epxress server start');
 });
+
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(session({
+    secret: '!secretkey#', // 쿠키 변조 방지를 위한 sign 값, 원하는 값 할당
+    resave: false, // 세션을 언제나 저장할 지 정하는 값
+    saveUninitialized: true // 
+}));
+
+var router = require('./router/main') (app);
